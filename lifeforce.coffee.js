506d78587cf494fdfe4496ab8b4ec44a272504bc
joshuacc <josh@designpepper.com>
2011-11-11 22:00:33 -0500

    Initial setup for the canvas.

--- lifeforce.coffee.js
+++ lifeforce.coffee.js
@@ -1,1 +1,7 @@
-console.log test
+# Initial setup
+
+l_canvas = document.getElementbyID "life"
+l_context = l_canvas.getContext "2d"
+
+l_context.fillStyle = "#fff"
+l_context.fillRect 0, 0, 640, 360
