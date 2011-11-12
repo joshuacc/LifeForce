8c6ecd2cc886dc164c701c007dcf6abde05c5399
joshuacc <josh@designpepper.com>
2011-11-12 11:45:15 -0500

    Initialize with random cell states.

--- lifeforce.coffee.js
+++ lifeforce.coffee.js
@@ -6,10 +6,13 @@ l_context = l_canvas.getContext "2d"
 l_context.fillStyle = "#fff"
 l_context.fillRect 0, 0, 640, 360
 
-# Initialize a 2d array as entirely dead
+# Initialize a 2d array
 # False represents a "dead" cell, while true represents a "live" cell
 l =
 	for x in [0..639]
-		false for y in [0..359]
+		for y in [0..359]
+			randomnum = Math.floor(Math.random() * 2)
+			!!randomnum
 
 console.log l
+
