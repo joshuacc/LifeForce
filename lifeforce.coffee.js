aaa113783af36a81538fa6a08ed27cb0bcbdb1d6
joshuacc <josh@designpepper.com>
2011-11-12 10:05:53 -0500

    Some basic initialization to test out.

--- lifeforce.coffee.js
+++ lifeforce.coffee.js
@@ -5,3 +5,11 @@ l_context = l_canvas.getContext "2d"
 
 l_context.fillStyle = "#fff"
 l_context.fillRect 0, 0, 640, 360
+
+# Initialize a 2d array as entirely dead
+# False represents a "dead" cell, while true represents a "live" cell
+l =
+	for x in [0..639]
+		false for y in [0..359]
+
+console.log l
