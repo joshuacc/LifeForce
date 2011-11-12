52c08ac8f78a52027332607460f9271c42960bc8
joshuacc <josh@designpepper.com>
2011-11-12 12:14:42 -0500

    Quick refactoring.

--- lifeforce.coffee.js
+++ lifeforce.coffee.js
@@ -1,10 +1,7 @@
-# Initial setup
+# Initialize world canvas
 
 l_canvas = document.getElementById "life"
 l_context = l_canvas.getContext "2d"
-
-l_context.fillStyle = "#fff"
-l_context.fillRect 0, 0, 640, 360
 
 # Initialize a 2d array
 # False represents a "dead" cell, while true represents a "live" cell
@@ -14,14 +11,14 @@ world =
 			randomnum = Math.floor(Math.random() * 2)
 			!!randomnum
 
-console.log world
-
 # Paint the initial world state
-for x in [0..639]
-	for y in [0..359]
-		if world[x][y] == true
-			l_context.fillStyle = "#000"
-		else
-			l_context.fillStyle = "#fff"
-		l_context.fillRect x, y, 1, 1
+drawworld = ->
+	for x in [0..639]
+		for y in [0..359]
+			if world[x][y] == true
+				l_context.fillStyle = "#000"
+			else
+				l_context.fillStyle = "#fff"
+			l_context.fillRect x, y, 1, 1
 
+drawworld
