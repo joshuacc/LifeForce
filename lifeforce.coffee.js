1e3c68fb60d8b16aad2279b720ade088350c83c2
joshuacc <josh@designpepper.com>
2011-11-12 11:54:41 -0500

    Painting the initial world state

--- lifeforce.coffee.js
+++ lifeforce.coffee.js
@@ -8,11 +8,20 @@ l_context.fillRect 0, 0, 640, 360
 
 # Initialize a 2d array
 # False represents a "dead" cell, while true represents a "live" cell
-l =
+world =
 	for x in [0..639]
 		for y in [0..359]
 			randomnum = Math.floor(Math.random() * 2)
 			!!randomnum
 
-console.log l
+console.log world
+
+# Paint the initial world state
+for x in [0..639]
+	for y in [0..359]
+		if l[x][y] == true
+			l_context.fillStyle = "#000"
+		else
+			l_context.fillStyle = "#fff"
+		l_context.fillRect x, y, 1, 1
 
