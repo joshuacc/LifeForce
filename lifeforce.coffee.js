a2fd651adef5a9870976c9017003733439c2e040
joshuacc <josh@designpepper.com>
2011-11-18 18:09:49 -0500

    Added code to update the force canvas.

--- lifeforce.coffee.js
+++ lifeforce.coffee.js
@@ -22,10 +22,15 @@ drawworld = ->
 	for x in [0..639]
 		for y in [0..359]
 			if world[x][y] == true
+				f_context.fillStyle = "rgba(0,0,0,0.0039)"
 				l_context.fillStyle = "#000"
 			else
+				f_context.fillStyle = "rgba(255,255,255,0.0039)"
 				l_context.fillStyle = "#fff"
 			l_context.fillRect x, y, 1, 1
+
+			f_context.fillStyle = "rgba(255,255,255,0.0039)"
+			f_context.fillRect x, y, 1, 1
 
 drawworld()
 
